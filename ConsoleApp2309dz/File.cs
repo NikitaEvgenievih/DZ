115babb2a9466a84e67bd37809bb57b418f65204
NikitaEvgenievih <nikitabykov2006hag@gmail.com>
2022-09-23 18:19:12 +0300

    DZ commit

---
+++ ConsoleApp2309dz/File.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2309dz
+{
+    public class File
+    {
+        public string Name { get; set; }
+        public string Tip { get; set; }
+        public double Size { get; set; }
+        public DateTime Date { get; set; }
+
+        public File()
+        {
+            Name = "Defult";
+            Tip = "Defult";
+            Size = 0;
+            Date = DateTime.Now;
+        }
+
+        public File(string name, string tip, double size, DateTime date)
+        {
+            Name = name;
+            Tip = tip;
+            Size = size;
+            Date = date;
+        }
+
+        public string Convertor()
+        {
+            double convert_size;
+
+            if (Size > 0 || Size < 1000)
+            {
+                return Size.ToString();
+            }
+            else if (Size >= 1000 || Size < 1000000)
+            {
+                convert_size = Size / 1000;
+                return convert_size.ToString();
+            }
+            else if(Size >= 1000000 || Size < 1000000000)
+            {
+                convert_size = Size / 1000000;
+                return convert_size.ToString();
+            }
+            else if(Size >= 1000000000 || Size < 1000000000000)
+            {
+                convert_size = Size / 1000000000;
+                return convert_size.ToString();
+            }
+            string str = "";
+            return str;
+            
+        }
+    }
+}
---
+++ ConsoleApp2309dz/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp2309dz;
+using File = ConsoleApp2309dz.File;
+
+File[] files = new File[]
+{
+    new File ("nikita", ".txt", 954, DateTime.Now),
+    new File ("436.ua", ".json", 47295301954, DateTime.Now),
+    new File ("Files Sonia", ".json", 95425094, DateTime.Now),
+    new File ("rabota", ".txt", 954654, new DateTime(2015, 7, 20)),
+    new File ("refetat math", ".doxs", 96546735665, DateTime.Now),
+};
+
+foreach (File item in files)
+{
+    if(item.Tip == ".txt")
+    {
+        Console.WriteLine(item.Name);
+    }
+}
+
+foreach (File item in files)
+{
+    if (item.Name.EndsWith("ua"))
+    {
+        Console.WriteLine(item.Name);
+    }
+}
+
+foreach (File item in files)
+{
+    DateTime OurDate = new DateTime(2022, 9, 17);
+    if (item.Date < OurDate)
+    {
+        Console.WriteLine(item.Name);
+    }
+}
